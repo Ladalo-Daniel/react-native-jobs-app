764bfabbe2c280d670236abaa2f84f7e08b61226
Ladalo-Daniel <balaladalo@gmail.com>
2024-07-01 13:49:03 +0100

    feat: job details and pagination added

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { Image, TouchableOpacity } from "react-native";
 
-import styles from './screenheader.style'
+import styles from "./screenheader.style";
 
-const ScreenHeaderBtn = ({iconUrl, dimention, handlePress}) => {
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
   return (
     <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
-       <Image 
-       source={iconUrl}
-       resizeMode='cover'
-       style={styles.btnImg(dimention)}
-        />
+      <Image
+        source={iconUrl}
+        resizeMode='cover'
+        style={styles.btnImg(dimension)}
+      />
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default ScreenHeaderBtn
+export default ScreenHeaderBtn;
---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": '1ba7af6d43msh229b98cb8f2a51fp14c309jsn760eb60920f7',
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
