2519d3a32acf747e1859fa64cbf2b2dbf38d0a96
Ladalo-Daniel <balaladalo@gmail.com>
2024-07-01 14:13:55 +0100

    feat: job details and pagination added

--- components/common/cards/popular/popularjobcard.style.js
+++ components/common/cards/popular/popularjobcard.style.js
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
   container: (selectedJob, item) => ({
     width: 250,
     padding: SIZES.xLarge,
-    backgroundColor: selectedJob === item.job_id ? COLORS.primary : "#FFF",
+    backgroundColor: selectedJob === item.job_id ? COLORS.primary : "#2e1065",
     borderRadius: SIZES.medium,
     justifyContent: "space-between",
     ...SHADOWS.medium,
